76f4861c57138c6285e1ac6131158fb6a5f9369a
danieltb92 <daniel.tunjano92@gmail.com>
2024-02-12 21:50:57 -0500

    add hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,27 @@
 import logo from '../assets/images/RockTicket_Logo.png';
 import googlePlay from '../assets/images/1.png';
 import appStore from '../assets/images/2.png';
+import '../css/hero.css';
+
+
 
 function Header() {
 
     return (
-        <div>
-            <img src={logo} alt="Logo RockTicket" />
-            <h1> Dejate contagiar <br></br>por el
-              <span > Rock.</span>
+        <div className="hero flex justify-center flex-col items-center">
+            <img className="w-50" src={logo} alt="Logo RockTicket" />
+            <h1 className="text-7xl text-center tracking-wide leading-90 pt-16 pb-3"> Dejate contagiar <br></br>por el
+              <span className="text-sky-300"> Rock.</span>
             </h1>
-            <h4>La App que te lleva a los mejores conciertos de rock en tu ciudad.<br></br>
+            <h4 className="hero-h4 text-lg leading-5 text-center tracking-wide m-4">
+                La App que te lleva a los mejores conciertos de rock en tu ciudad.<br></br>
                 Descarga la app  y dejate contagiar de la energia del rock.</h4>
-            <section>
-                <a href="">
-                    <img src={googlePlay} alt="boton google play" />
+            <section className="flex justify-center m-6 gap-3">
+                <a href="https://play.google.com/" target="_blank" rel="noopener">
+                    <img className="h-10" src={googlePlay} alt="boton google play" />
                 </a>
-                <a href="">
-                    <img src={appStore} alt="boton app store" />
+                <a href="https://www.apple.com/co/app-store/" target="_blank" rel="noopener">
+                    <img className="h-10 " src={appStore} alt="boton app store" />
                 </a>
             </section>
         </div>
