5a6d7f971da194d55d506043fd01fd54e5f64266
danieltb92 <daniel.tunjano92@gmail.com>
2024-03-11 13:23:14 -0500

    fix: hero-h4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,10 @@ function Hero() {
             <div className=' hero z-0 top-0 flex justify-center'></div>
             <div className=" z-10 top-0 absolute w-screen h-screen flex justify-center flex-col items-center">
                 <img className="w-48" src={logo} alt="Logo RockTicket" />
-                <h1 className="text-6xl lg:text-8xl md:text-7xl text-center tracking-wide leading-[80%] pt-16 pb-3 mx-10"> Dejate contagiar <br></br>por el
+                <h1 className="text-6xl lg:text-8xl md:text-7xl text-center tracking-wide leading-[80%] pt-16 pb-2 mx-10"> Dejate contagiar <br></br>por el
                     <span className="text-sky-300"> Rock.</span>
                 </h1>
-                <h4 className="font-roboto hero-h4 hero-h4-lg text-xs leading-5 text-center tracking-[1.6px] m-10">
+                <h4 className="font-roboto hero-h4 hero-h4-lg text-xs leading-5 text-center tracking-[1.6px] mb-10 mx-2">
                     La App que te lleva a los mejores conciertos de rock en tu ciudad.<br></br>
                     Descarga la app  y dejate contagiar de la energia del rock.</h4>
                 <section className="flex justify-center m-6 gap-3">
